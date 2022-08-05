570b52cb723e66260c25a41155db8b3400ccaeed
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-08-05 17:16:01 -0300

    feat: service validation process

--- back-end/src/database/services/login.js
+++ back-end/src/database/services/login.js
@@ -2,9 +2,40 @@ const { User } = require('../models')
 
 const loginService = async (email) => {
   const user = await User.findOne({ where: { email } });
+
+  return user;
+}
+
+const loginProcess = async (email) => {
+  const user = await loginService(email);
+  const payload = {
+    email: user.email,
+    role: user.role,
+  };
+
+  const token = JWT.createToken(payload); // o payload deve ser um objeto;
+
+  return { token };
+}
+
+const verifyToken = (auth) => {
+  const code = JWT.decodeToken(auth);
+
+  return code;
+}
+
+const encryptor = async (password, email) => {
+  const user = await User.findOne({ where: { email } });
+
+  if (user) {
+    return encryptor.compare(password, user.password);
+  }
 }
 
 module.exports = {
   loginService,
+  loginProcess,
+  verifyToken,
+  encryptor,
 };
 
