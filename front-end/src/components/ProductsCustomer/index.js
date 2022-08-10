1466f0bd775e15667f53f4c3291856625c30bbd3
MarcsVinicius <marcos.vgalencar@gmail.com>
2022-08-10 15:09:33 -0300

    feat: created component product

---
+++ front-end/src/components/ProductsCustomer/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function costumerProducts({ name, price }) {
+  return (
+    <h1>componente de produtos</h1>
+  );
+}
--- front-end/src/pages/Products/Products.jsx
+++ front-end/src/pages/Products/Products.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CostumerProducts from '../../components/ProductsCustomer';
 
 function Products() {
+  function getProducts() {
+    const products = fetch(`${process.env.REACT_APP_BASE_URL}/products`)
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((error) => error);
+    return products;
+  }
   return (
     <>
+      <CostumerProducts />
       tela de produtos
     </>
   );
