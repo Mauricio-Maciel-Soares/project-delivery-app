cff23f2b3a581f84efa3dd66c09f75c59d47fc74
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-08-10 15:13:44 -0300

    branch change

--- back-end/src/database/controllers/register.js
+++ back-end/src/database/controllers/register.js
@@ -1,17 +1,17 @@
 const {
   registerProcess,
-  registerEmail,
-  registerName,
+  registerValidate,
 } = require('../services/register')
 
 const registerController = async (req, res, _next) => {
   const dataBody = req.body;
 
-  const userEmail = await registerEmail(dataBody.email);
-  if(userEmail) return res.status(409).json({ message: 'The email already registered' });
+  const isUserRegistered = await registerValidate();
+  const filteredEmail = isUserRegistered.filter((e) => e.email === dataBody.email);
+  if(filteredEmail) return res.status(409).json({ message: 'The email already registered' });
 
-  const userName = await registerName(dataBody.name);
-  if(userName) return res.status(409).json({ message: 'The name already registered' });
+  const filteredName = isUserRegistered.filter((e) => e.name === dataBody.name);
+  if(filteredName) return res.status(409).json({ message: 'The name already registered' });
 
   const newUser = await registerProcess(dataBody);
   return res.status(201).json(newUser);
