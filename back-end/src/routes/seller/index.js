c98fa418dc6e7edd2846260e772f0f97902ed41d
Dayana <dayanadiasgarcia@gmail.com>
2022-08-16 15:07:07 -0300

    router seller order details

--- back-end/src/routes/seller/index.js
+++ back-end/src/routes/seller/index.js
@@ -4,5 +4,6 @@ const sellerController = require('../../database/controllers/seller.controller')
 const sellerRouter = express.Router();
 
 sellerRouter.get('/:id/orders', sellerController.sellerOrder);
+sellerRouter.get('/:id/orders/details', sellerController.sellerOrderDetails)
 
 module.exports = sellerRouter;
