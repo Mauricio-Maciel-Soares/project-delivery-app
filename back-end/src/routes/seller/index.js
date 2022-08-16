02f56238118b060d0b0f3ef47722df068e38864a
Dayana <dayanadiasgarcia@gmail.com>
2022-08-16 15:50:27 -0300

    delete gitkeep

--- back-end/src/routes/seller/index.js
+++ back-end/src/routes/seller/index.js
@@ -4,6 +4,6 @@ const sellerController = require('../../database/controllers/seller.controller')
 const sellerRouter = express.Router();
 
 sellerRouter.get('/:id/orders', sellerController.sellerOrder);
-sellerRouter.get('/:id/orders/details', sellerController.sellerOrderDetails)
+sellerRouter.get('/:id/orders/:id', sellerController.sellerOrderDetails)
 
 module.exports = sellerRouter;
