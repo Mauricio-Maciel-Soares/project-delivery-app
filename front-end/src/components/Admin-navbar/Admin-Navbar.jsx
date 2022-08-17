27b13f65e329e1841b5d361da04ac20fbeeb9a4f
Dayana <dayanadiasgarcia@gmail.com>
2022-08-17 17:15:21 -0300

    add nav bar to admin page

---
+++ front-end/src/components/Admin-navbar/Admin-Navbar.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function AdminNavbar() {
+  const [adminName, setAdminName] = useState('');
+
+  const history = useHistory();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/login');
+  };
+
+  useEffect(() => {
+    const adminData = JSON.parse(localStorage.getItem('admin')) || {};
+    setAdminName(adminData.name);
+
+    const isTokenValid = verifyToken(adminData.token);
+
+    if (!isTokenValid) handleLogout();
+  }, []);
+
+  return (
+    <header>
+      <span>
+        GERENCIAR USUÁRIOS
+      </span>
+
+      <span>
+        {adminName}
+      </span>
+
+      <button
+        type="button"
+        onClick={ handleLogout }
+      >
+        Sair
+      </button>
+    </header>
+  );
+}
