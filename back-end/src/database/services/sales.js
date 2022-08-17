07e2e7bdcfcb53b98901b253df1b4ac913713e9a
rolwane <rolwane@hotmail.com>
2022-08-17 15:25:26 -0300

    fix sales router

--- back-end/src/database/services/sales.js
+++ back-end/src/database/services/sales.js
@@ -4,17 +4,19 @@ const seedSalesProducts = async (dataBody, newSale) => {
   const products = dataBody.products;
 
   products.forEach( async (e) => {
-      await salesProduct.create({
-        quantity: e.quantity,
-        saleId: newSale.id,
-        productId: e.productId,
-      })
+    await salesProduct.create({
+      quantity: e.quantity,
+      saleId: newSale.id,
+      productId: e.id,
     });
+  });
 };
 
 const createSale = async (dataBody) => {
   const today = new Date(Date.now())
+
   const saleDate = today.toUTCString();
+
   const newSale = await sale.create({
     userId: dataBody.userId,
     sellerId: dataBody.sellerId,
